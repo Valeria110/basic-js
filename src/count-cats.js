e8810036bfe4599029a6bf4aa47ba95874b2a505
Valeriya Ivashkevich <ivashkevich03@gmail.com>
2023-12-14 17:42:35 +0300

    feat: add count-cats and recursive-depth tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  const arr = matrix.flat(Infinity);
+  let count = 0;
+
+  arr.forEach(item => {
+    if (item === '^^') {
+      count ++;
+    }
+  })
+
+  return count;
 }
 
 module.exports = {

080867a99c17ce52431519284d4d108404dda514
Valeriya Ivashkevich <ivashkevich03@gmail.com>
2023-12-15 18:26:14 +0300

    feat: add calculateHanoi() task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed) {
+  // for 8 disks, the puzzle requires (2 ** 8) − 1, or 255 transfers (from the article)
+  const turns = 2 ** disksNumber - 1;
+  // 3600 сек. - turnsSpeed
+  // х сек.    - turns
+  // х сек. = (turns * 3600) / turnsSpeed
+  const seconds = Math.floor((turns * 3600) / turnsSpeed);
+  const puzzleScore = {
+    turns: turns,
+    seconds: seconds
+  }
+
+  return puzzleScore;
 }
 
 module.exports = {
